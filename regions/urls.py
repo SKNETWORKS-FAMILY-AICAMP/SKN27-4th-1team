7e169ceb313a8de74dc32814ccf9a49365d74a9d
nanseong <nanseong>
2026-06-05 16:12:18 +0900

    link urls, add button strorymaker

--- regions/urls.py
+++ regions/urls.py
@@ -5,6 +5,7 @@ app_name = 'regions'
 
 urlpatterns = [
     path('jidogam/', views.jidogam, name='jidogam'),
+    path('jidogam.html', views.jidogam, name='jidogam_html'),
     path('regioninfo/', views.jidogam, name='regioninfo'),
     path('list/', views.jidogam, name='list'),
     path('<str:region_name>/', views.jidogam, name='detail'),
