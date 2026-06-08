35160f01966306b8a50ce8e889873ac7c37fb4a0
nanseong <nanseong>
2026-06-08 11:35:24 +0900

    add nav-bar & log in button,  top-bar visual alignment,  eidit my page

--- post/urls.py
+++ post/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('delete/<int:post_id>/', views.post_delete_api, name='delete'),
     path('api/create/', views.post_create_api, name='post_create_api'),
     path('api/list/', views.post_list_api, name='post_list_api'),
+    path('api/search/', views.post_search_api, name='post_search_api'),
     path('api/detail/<int:post_id>/', views.post_detail_api, name='post_detail_api'),
     path('api/like/<int:post_id>/', views.post_like_api, name='post_like_api'),
     path('api/edit/<int:post_id>/', views.post_edit_api, name='post_edit_api'),
