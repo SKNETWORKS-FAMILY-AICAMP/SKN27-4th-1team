693077118ff1976cfa8cf457e07d5e570fe88e57
SongwonPark08 <songwonpark87@gmail.com>
2026-06-08 17:04:20 +0900

    프롬프트 수정

--- common/llm_factory.py
+++ common/llm_factory.py
@@ -1,5 +1,6 @@
 from langchain_groq import ChatGroq
 #from langchain_ollama import OllamaLLM
+from langchain_ollama import OllamaLLM
 
 def get_post_generation_llm():
     return ChatGroq(
@@ -9,3 +10,6 @@ def get_post_generation_llm():
 
 def get_llm(): # evaluation, rag
     return ChatGroq(model="openai/gpt-oss-120b")
+
+def generator_llm():
+    return OllamaLLM(model="gemma4:e4b")
